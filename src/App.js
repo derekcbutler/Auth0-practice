77ad20f60953205f8be57eae078cde6762430c6d
Derek Butler <derekbutler@Dereks-MacBook-Pro.local>
2019-11-20 15:16:57 -0700

    lolGotcha

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
+
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Callback from './Component/Callback';
+import Home from './Component/Home';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <Route exact path='/' component={Home} />
+      <Route path='/callback' component={Callback} exact />
     </div>
-  );
+  )
 }
 
 export default App;
